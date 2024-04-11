0491c75900773e2b0e07811006a8c6fed7050674
KhanhHG <honggiakhanh@gmail.com>
2024-04-11 20:09:06 +0300

    language picker restyling

--- src/components/LanguagePicker.tsx
+++ src/components/LanguagePicker.tsx
@@ -12,22 +12,24 @@ type Props = {};
 
 const LanguagePicker = (props: Props) => {
   return (
-    <Select defaultValue="en">
-      <SelectTrigger id="languages" className="w-30 space-x-2">
-        <SelectValue placeholder="Lang" />
-      </SelectTrigger>
-      <SelectContent position="popper">
-        <SelectItem value="en">
-          <span className="fi fis fi-gb pr-2"></span>
-        </SelectItem>
-        <SelectItem value="fi">
-          <span className="fi fis fi-fi"></span>
-        </SelectItem>
-        <SelectItem value="vn">
-          <span className="fi fis fi-vn"></span>
-        </SelectItem>
-      </SelectContent>
-    </Select>
+    <div>
+      <Select defaultValue="en">
+        <SelectTrigger id="languages" className="space-x-2">
+          <SelectValue placeholder="Lang" />
+        </SelectTrigger>
+        <SelectContent position="popper">
+          <SelectItem value="en">
+            <span className="fi fis fi-gb pr-2"></span>
+          </SelectItem>
+          <SelectItem value="fi">
+            <span className="fi fis fi-fi"></span>
+          </SelectItem>
+          <SelectItem value="vn">
+            <span className="fi fis fi-vn"></span>
+          </SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
   );
 };
 
