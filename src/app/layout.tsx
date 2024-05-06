7786f0ffb4c1a6dc4190d84bf278d1c7bd7ac220
KhanhHG <honggiakhanh@gmail.com>
2024-05-06 18:03:35 +0300

    dark/light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Container from "@/components/Container";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,12 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <Container>
-          <Header />
-          {children}
-        </Container>
+        <Providers>
+          <Container>
+            <Header />
+            {children}
+          </Container>
+        </Providers>
       </body>
     </html>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,8 @@ import {
 } from "./ui/select";
 import Link from "next/link";
 import LanguagePicker from "./LanguagePicker";
+import ThemeSwitch from "./ThemeToggler";
+import ThemeToggler from "./ThemeToggler";
 
 type Props = {};
 
@@ -18,18 +20,7 @@ const Header = (props: Props) => {
         <Link href="/" className="text-xl font-bold">
           BoardGameFInder
         </Link>
-        <Select>
-          <SelectTrigger id="categories" className="space-x-2">
-            <SelectValue placeholder="Categories" />
-          </SelectTrigger>
-          <SelectContent position="popper">
-            <SelectItem value="strategy">Strategy</SelectItem>
-            <SelectItem value="family">Family</SelectItem>
-            <SelectItem value="party">Party</SelectItem>
-            <SelectItem value="card">Card</SelectItem>
-            <SelectItem value="dice">Dice</SelectItem>
-          </SelectContent>
-        </Select>
+        <ThemeToggler />
       </div>
       <div className="flex items-center space-x-10">
         <Link className="text-sm" href="#">
