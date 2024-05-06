722779a56b9dc1bf0a6e8cbba96b7fd8ae49eabe
KhanhHG <honggiakhanh@gmail.com>
2024-05-06 21:12:30 +0300

    main page text change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
     <div className="max-w-screen-xl mx-auto px-4">
       <main>
         <section className="text-center py-12">
-          <h1 className="text-3xl font-bold mb-4">TableTopFinder</h1>
+          <h1 className="text-3xl font-bold mb-4">BoardGameFinder</h1>
           <p className="max-w-md mx-auto mb-6">
-            TableTopFInder is a Finnish board game finder that will help you
+            BoardGameFinder is a Finnish board game finder that will help you
             discover your favorite tabletop games that Finnish retailers have
             available. Start by using the search bar below or click on any board
             game below to start your board game finding quest.
