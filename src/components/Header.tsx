a407aa063f9985b9954a578a704a26860bca0465
KhanhHG <honggiakhanh@gmail.com>
2024-05-18 18:10:22 +0300

    auth setup with authjs + google provider

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,16 @@
 import React from "react";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "./ui/select";
 import Link from "next/link";
 import LanguagePicker from "./LanguagePicker";
-import ThemeSwitch from "./ThemeToggler";
 import ThemeToggler from "./ThemeToggler";
+import { auth, signOut } from "@/lib/auth/auth";
+import { Button } from "./ui/button";
 
 type Props = {};
 
-const Header = (props: Props) => {
+const Header = async (props: Props) => {
+  const session = await auth();
+  const user = session?.user;
+
   return (
     <header className="flex justify-between items-center py-4">
       <div className="flex items-center space-x-4">
@@ -23,9 +20,23 @@ const Header = (props: Props) => {
         <ThemeToggler />
       </div>
       <div className="flex items-center space-x-10">
-        <Link className="text-sm" href="#">
-          Login / Create account
-        </Link>
+        {user ? (
+          <>
+            <div>{user.name}</div>
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <Button>Sign Out</Button>
+            </form>
+          </>
+        ) : (
+          <Link className="text-sm" href="/api/auth/">
+            Login / Create account
+          </Link>
+        )}
         <LanguagePicker />
       </div>
     </header>
